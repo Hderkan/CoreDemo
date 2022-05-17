003ee45f92afcf924155714fb8ffc9dc44a82e5f
Hderkan <hakanderkan@outlook.com>
2022-05-17 23:43:38 +0300

    düzenleme
    düzenleme

--- EntityLayer/Concrete/Blog.cs
+++ EntityLayer/Concrete/Blog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EntityLayer.Concrete
 {
     public class Blog
     {
+        [Key]
         public int BlogID { get; set; }
 
         public string BlogTitle { get; set; }
@@ -18,9 +20,14 @@ namespace EntityLayer.Concrete
 
         public string BlogImage { get; set; }
 
-        public string BlogCreateDate { get; set; }
+        public DateTime BlogCreateDate { get; set; }
 
         public bool BlogStatus { get; set; }
+        public int CategoryID { get; set; }
+
+        public Category Category { get; set; }
+
+        public List<Comment> Comments { get; set; }
 
 
     }
---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentID { get; set; }
+
+        public string CommentUserName { get; set; }
+
+        public string ComentContent { get; set; }
+        public string ComentTitle { get; set; }
+
+        public DateTime CommentDate { get; set; }
+
+        public bool CommentStatus { get; set; }
+
+        public int BlogID { get; set; }
+
+        public Blog Blog { get; set; }
+
+
+
+
+    }
+}
